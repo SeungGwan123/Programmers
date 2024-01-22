74fbc60877295b8433ee2545033a8a487669b6bc
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-01-23 01:54:14 +0900

    [Gold III] Title: 우주 탐사선, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
+++ 백준/Gold/17182. 우주 탐사선/우주 탐사선.cc
@@ -27,7 +27,6 @@ int main(){
             int temp;
             cin>>temp;
             dis[i].push_back(temp);
-            //if(i==start) fly.push_back({temp,start,j});
         }
     }
     for(int a=0;a<planet;a++) {
@@ -36,46 +35,10 @@ int main(){
                 if (i == j)continue;
                 if (i == a || j == a)continue;
                 dis[i][j] = min(dis[i][j], dis[i][a] + dis[a][j]);
-                //if(i==start) get<0>(fly[j])=dis[i][j];
             }
         }
     }
     visited[start]=true;
     dfs(start,0,1);
-//    for(int j=0;j<dis[start].size();j++) {
-//        if(start==j)continue;
-//        int visit = 1;
-//        int result = 0;
-//        visited[start] = true;
-//        fly.push_back({dis[start][j],start,j});
-//        //sort(fly.begin(), fly.end());
-//        int last_end = start;
-//        while (visit < planet) {
-//            int value;
-//            int start;
-//            int end;
-//            tie(value, start, end) = fly[0];
-//            fly.erase(fly.begin(), fly.begin() + 1);
-//            if (visited[end])continue;
-//            for (int i = 0; i < fly.size(); i++) {
-//                int v;
-//                int s;
-//                int e;
-//                tie(v, s, e) = fly[i];
-//                fly[i] = {v - dis[last_end][s], s, e};
-//                fly[i] = {v + dis[end][s], s, e};
-//            }
-//            visited[end] = true;
-//            result += value;
-//            visit++;
-//            last_end = end;
-//            for (int i = 0; i < dis[end].size(); i++) {
-//                if (visited[i])continue;
-//                fly.push_back({dis[end][i], end, i});
-//            }
-//            sort(fly.begin(), fly.end());
-//        }
-//        final_result = min(final_result, result);
-//    }
     cout << final_result;
 }
