6b03223cda1b6c4b064f7f25d338be53226d4353
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-04-08 12:56:06 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
 SELECT b.title as TITLE, r.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, date_format(r.CREATED_DATE,'%Y-%m-%d') as CREATED_DATE
 from used_goods_board as b, used_GOODS_REPLY as r
-where (DATE(b.created_date) between '2022-10-01' and '2022-10-31') and b.board_id = r.board_id
+where (DATE(b.created_date) >= '2022-10-01' and DATE(b.created_date) <='2022-10-31') 
+and b.board_id = r.board_id
 order by r.created_date asc, b.title asc
