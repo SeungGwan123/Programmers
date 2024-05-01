78fdd41f6a1e14fab1ae31d81b2c92ca59dd27ed
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-05-01 19:35:09 +0900

    [Silver V] Title: CD, Time: 536 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Silver/4158. CD/CD.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    while(true) {
+        int n;
+        int m;
+        cin >> n >> m;
+        if(n==0&&m==0)break;
+        vector<int> sg;
+        vector<int> sy;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            int temp;
+            cin >> temp;
+            sg.push_back(temp);
+        }
+        for (int i = 0; i < m; i++) {
+            int temp;
+            cin >> temp;
+            sy.push_back(temp);
+        }
+        int g=0;
+        int y=0;
+        while(g<sg.size()&&y<sy.size()){
+            if(sg[g]==sy[y]){
+                g++;
+                y++;
+                result++;
+            }else{
+                if(sg[g]>sy[y]) y++;
+                else g++;
+            }
+        }
+        cout<<result<<endl;
+    }
+}
