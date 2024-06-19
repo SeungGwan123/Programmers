eebd4ca08591ab11228997e97379b347af816409
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-06-19 22:53:33 +0900

    [level 2] Title: 문자열 압축, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.js
@@ -1,40 +1,35 @@
 function solution(s) {
     let answer = s.length;
     for(let i=1;i<=s.length/2;i++){
+        let words = [];
+        for(let j=0;j<s.length;j+=i){
+            words.push(s.slice(j,j+i));
+        }
+        //console.log(words[0].length);
+        let now = 0;
         let count = 0;
-        let temp = '';
-        for(let j=0;j<s.length;j++){
-            let m=0;
-            let n=j+i;
-            let sub_count = i;
-            while(true){
-                if(s.charAt(j+m)!==s.charAt(n)) break;
-                m++;
-                n++;
-                if(m===i) {
-                    sub_count+=i;
-                    m=0;
+        let num = 1;
+        for(let j=1;j<words.length;j++){
+            if(words[now]!==words[j]){
+                if(num>=1000) count+=words[now].length+4;
+                else if(num>=100) count+=words[now].length+3;
+                else if(num>=10) count+=words[now].length+2;
+                else if(num===1) count+=words[now].length;
+                else count+=words[now].length+1;
+                now = j;
+                num=1;
+            }else num++;
+            if(j===words.length-1){
+                if(num===1) count+=words[now].length;
+                else{
+                    if(num>=1000) count+=words[now].length+4;
+                    else if(num>=100) count+=words[now].length+3;
+                    else if(num>=10) count+=words[now].length+2;
+                    else count+=words[now].length+1;
                 }
             }
-            if(sub_count>i){
-                if(sub_count/i>=1000) count += i+4;
-                else if(sub_count/i>=100) count += i+3;
-                else if(sub_count/i>=10) count += i+2;
-                else count += i+1;
-                
-                temp+=(sub_count/i);
-                temp+=s.charAt(j);
-                
-            }else{
-                if(j+i>=s.length) count+=(s.length-j);
-                else count+=i;
-                temp+=s.charAt(j);
-            }
-            j+=(sub_count-1);
         }
-        answer = Math.min(answer,count);
-        temp+=' '+count;
-        //console.log(temp);
+        answer = Math.min(count,answer);
     }
     return answer;
 }
