319f5c5e3fbc46ed70edfc2d2dbe812ffa378500
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2024-10-22 15:50:48 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,12 +1,16 @@
--- 코드를 입력하세요
-with max_price as(
-    select category, product_name, max(price) price
+select a.category as CATEGORY, a.price as MAX_PRICE, PRODUCT_NAME
+from food_product a, 
+(
+    select CATEGORY, max(price) as MAX_PRICE
     from food_product
-    where category in ('과자','국','김치','식용유')
     group by category
-)
+    having category in ('과자','국','김치','식용유')
+ ) as b
+where a.price = b.max_price and a.category = b.category
+order by price desc
 
-select m.category CATEGORY, m.price MAX_PRICE, fp.product_name PRODUCT_NAME
-from max_price m, food_product fp
-where m.category = fp.category and m.price = fp.price
-order by m.price desc
+
+# P0051	맛있는배추김치	CD_KC00001	김치	19000
+# P0014	맛있는마조유	CD_OL00004	식용유	8950
+# P0074	맛있는김치찌개	CD_SU00004	국	2900
+# P0093	맛있는허니버터칩	CD_CK00003	과자	1950
