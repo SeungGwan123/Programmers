73836bd2452c1fd6e3e22984926f86bfe6c9d9da
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2025-08-07 22:47:08 +0900

    [level 1] Title: 신고 결과 받기, Time: 188.01 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,29 +1,19 @@
 function solution(id_list, report, k) {
-    var answer = [];
-    let notice = new Set();
-    let check = new Map();
-    let count = new Map();
-    id_list.forEach(id=>{
-        check.set(id,[]);
-        count.set(id,0);
+    var answer = Array(id_list.length).fill(0);
+    const r = new Map(id_list.map((id,idx)=> [id,idx]));
+    const s = [];
+    id_list.forEach(()=>s.push(new Set()))
+    report.forEach((p)=>{
+        const[start,end] = p.split(" ")
+        const num = r.get(end)
+        s[num].add(start);
     })
-    report.forEach(r=>{
-        let temp = r.split(' ');
-        if(!notice.has(temp.join())){
-            notice.add(temp.join());
-            count.set(temp[1],count.get(temp[1])+1);
-            let email = check.get(temp[0]);
-            email.push(temp[1]);
-            check.set(temp[0],email);
+    s.forEach((ss)=>{
+        if(ss.size>=k){
+            for(const sss of ss){
+                answer[r.get(sss)]++
+            }
         }
-    })
-    id_list.forEach(id=>{
-        let temp = check.get(id);
-        let temp_count = 0;
-        temp.forEach(t=>{
-            if(count.get(t)>=k) temp_count++;
-        })
-        answer.push(temp_count);
     })
     return answer;
 }
