39380d999f792359619c0a679b4d0fc26bc9f679
이승관 <123438749+SeungGwan123@users.noreply.github.com>
2026-02-09 21:34:27 +0900

    [level 2] Title: 짝지어 제거하기, Time: 41.87 ms, Memory: 43.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,11 @@
+function solution(s)
+{
+    var answer = -1;
+    const stack = []
+    for (let i = 0; i < s.length; i++) {
+        const c = s[i];
+        if (stack.length && stack[stack.length - 1] === c) stack.pop();
+        else stack.push(c);
+    }
+    return stack.length === 0 ? 1 : 0;
+}
